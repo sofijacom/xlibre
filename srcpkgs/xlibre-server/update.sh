384f4d9df819beed2b0b4481d02c8cd674087f5e
Sofiya <sofija.p2018@gmail.com>
2025-11-04 12:48:24 +0300

    Update update.sh

--- srcpkgs/xlibre-server/update.sh
+++ srcpkgs/xlibre-server/update.sh
@@ -11,7 +11,7 @@ CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
-export SHA256=$(curl -L https://github.com/X11Libre/xserver/archive/refs/tags/${VERSION}xlibre-xserver-${version}.tar.gz.sha256 | cut -d ' ' -f1 )
+export SHA256=$(curl -L https://github.com/X11Libre/xserver/archive/refs/tags/xlibre-xserver-${version}.tar.gz.sha256 | cut -d ' ' -f1 )
 [[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
 
 envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template
