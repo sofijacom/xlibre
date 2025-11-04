274cef76391f24f90b3927f07777aa3bb54d6950
Sofiya <sofija.p2018@gmail.com>
2025-11-04 12:24:31 +0300

    Update

--- srcpkgs/xlibre-server/update.sh
+++ srcpkgs/xlibre-server/update.sh
@@ -5,13 +5,13 @@ printf "Checking latest version\n"
 __dir="$(dirname "${BASH_SOURCE[0]}")"
 
 LATEST_VERSION=$(gh release list --repo X11Libre/xserver --exclude-drafts --exclude-pre-releases --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
-export VERSION=${LATEST_VERSION#"xlibre-xserver"}
+export VERSION=${LATEST_VERSION#"v"}
 CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
 printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
-export SHA256=$(curl -L https://github.com/X11Libre/xserver/releases/download/${VERSION}/archive/refs/tags/xlibre-xserver-${version}.tar.gz.sha256 | cut -d ' ' -f1 )
+export SHA256=$(curl -L https://github.com/X11Libre/xserver/releases/download/${VERSION}/archive/refs/tags/${version}.tar.gz.sha256 | cut -d ' ' -f1 )
 [[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
 
 envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template
