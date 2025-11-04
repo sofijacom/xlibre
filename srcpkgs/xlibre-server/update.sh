8793dc184cd7677ddf0b4b5314ea9508af980c12
Sofiya <sofija.p2018@gmail.com>
2025-11-04 13:19:33 +0300

    Update update.sh

--- srcpkgs/xlibre-server/update.sh
+++ srcpkgs/xlibre-server/update.sh
@@ -4,16 +4,19 @@ printf "Checking latest version\n"
 
 __dir="$(dirname "${BASH_SOURCE[0]}")"
 
-LATEST_VERSION=$(gh release list --repo X11Libre/xserver --exclude-drafts --exclude-pre-releases --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
+LATEST_VERSION=$(gh release list --repo X11Libre/xserver --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
 export VERSION=${LATEST_VERSION#"v"}
 CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
 printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
-export SHA256=$(curl -L https://github.com/X11Libre/xserver/releases/download/xlibre-xserver-${VERSION}/archive/refs/tags/${version}.tar.gz.sha256 | cut -d ' ' -f1 )
+# No preprepped checksum files, need to download the binary and calculate it myself
+gh release download -R X11Libre/xserver --archive=tar.gz --output "xlibre-xserver-${VERSION}.tar.gz"
+export SHA256=$(sha256sum ./xlibre-xserver-${VERSION}.tar.gz | cut -d ' ' -f1 )
+rm ./xlibre-xserver-${VERSION}.tar.gz
 [[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
 
 envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template
 
-printf "Xlibre template updated\n"
+printf "xlibre template updated\n"
