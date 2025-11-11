ca2e69d685ebe5743c3cab37572b87fdf1c61145
Sofiya <sofija.p2018@gmail.com>
2025-11-12 02:11:52 +0300

    Update

--- srcpkgs/xlibre-xf86-input-wacom/update.sh
+++ srcpkgs/xlibre-xf86-input-wacom/update.sh
@@ -4,8 +4,8 @@ printf "Checking latest version\n"
 
 __dir="$(dirname "${BASH_SOURCE[0]}")"
 
-LATEST_VERSION=$(gh release list --repo X11Libre/xf86-input-wacom/releases/tag/ --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
-export VERSION=${LATEST_VERSION#"xlibre-xf86-input-wacom-"}
+LATEST_VERSION=$(gh release list --repo X11Libre/xf86-input-wacom --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
+export VERSION=${LATEST_VERSION#"xlibre-xf86-video-wacom-"}
 CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
 printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
@@ -14,9 +14,9 @@ printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CU
 # No preprepped checksum files, need to download the binary and calculate it myself
 gh release download -R X11Libre/xserver --archive=tar.gz --output "${VERSION}.tar.gz"
 export SHA256=$(sha256sum ./${VERSION}.tar.gz | cut -d ' ' -f1 )
-#rm ./ ${VERSION}.tar.gz
+rm ./ ${VERSION}.tar.gz
 [[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
 
 envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template
 
-printf "xlibre-xf86-input-wacom template updated\n"
+printf "xlibre-xf86-video-wacom template updated\n"
