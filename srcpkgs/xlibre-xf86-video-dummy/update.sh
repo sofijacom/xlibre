4d1e49ad4fbe3b2213ea6fbac258697b150a9079
Sofiya <sofija.p2018@gmail.com>
2025-11-23 01:40:39 +0300

    Update

--- srcpkgs/xlibre-xf86-video-dummy/update.sh
+++ srcpkgs/xlibre-xf86-video-dummy/update.sh
@@ -4,19 +4,29 @@ printf "Checking latest version\n"
 
 __dir="$(dirname "${BASH_SOURCE[0]}")"
 
-LATEST_VERSION=$(gh release list --repo X11Libre/xf86-video-dummy --json name,tagName,isLatest --jq '.[] | select(.isLatest)|.tagName')
+LATEST_VERSION=$(git ls-remote --tags --refs --sort="v:refname" https://github.com/X11Libre/xf86-video-dummy xlibre-xf86-video-dummy-\* | tail -n1 | sed 's/.*\///')
 export VERSION=${LATEST_VERSION#"xlibre-xf86-video-dummy-"}
 CURRENT_VERSION=$(grep -E '^version=' ${__dir}/template | cut -d= -f2)
 
 printf "Latest version is: %s\nLatest built version is: %s\n" "${VERSION}" "${CURRENT_VERSION}"
 [ "${CURRENT_VERSION}" = "${VERSION}" ] && printf "No new version to release\n" && exit 0
 
+export VERSION=${VERSION%-*}
+
 # No preprepped checksum files, need to download the binary and calculate it myself
-gh release download -R X11Libre/xserver --archive=tar.gz --output "${VERSION}.tar.gz"
-export SHA256=$(sha256sum ./${VERSION}.tar.gz | cut -d ' ' -f1 )
-rm ./ ${VERSION}.tar.gz
+curl --fail -sL --output xlibre-xf86-video-dummy.tar.gz https://github.com/X11Libre/xf86-video-dummy/archive/refs/tags/${LATEST_VERSION}.tar.gz
+export SHA256=$(sha256sum ./xf86-video-dummy.tar.gz | cut -d ' ' -f1 )
+rm ./xf86-video-dummy.tar.gz
 [[ ! ${SHA256} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of sha256\n" && exit 1
 
-envsubst '${SHA256} ${VERSION}' < ${__dir}/.template > ${__dir}/template
+git clone --no-checkout --depth 1 --filter=blob:none --single-branch --branch ${LATEST_VERSION} https://github.com/X11Libre/xf86-video-dummy.git xf86-video-dummy
+cd xf86-video-dummy
+export GIT_SHORT_COMMIT=$(git rev-list --max-count=1 --abbrev-commit HEAD)
+cd ..
+rm -rf xf86-video-dummy
+[[ ! ${GIT_SHORT_COMMIT} =~ ^[a-z0-9]+$ ]] && printf "got junk instead of git short commit\n" && exit 1
+
+envsubst '${SHA256} ${VERSION} ${GIT_SHORT_COMMIT} ${TIMESTAMP}' < ${__dir}/.template > ${__dir}/template
 
 printf "xlibre-xf86-video-dummy template updated\n"
+
